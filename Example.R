e2ff0a3b07a3f833d3600ae144808bc565af84b9
FloSchuberth <f.schuberth@utwente.nl>
2022-10-06 10:36:07 +0200

    Add example of using the function

---
+++ Example.R
@@ -0,0 +1,21 @@
+library(lavaan)
+
+model='
+ PSS <~ PSS1+PSS2 +PSS3+ PSS4+ PSS5
+GRS<~GRS1+ GRS2+GRS3+GRS4+GRS5
+SRS<~SRS1+SRS2+SRS3+SRS4+SRS5
+
+PSS~~GRS + SRS
+GRS~~SRS
+'
+
+modelHO=specifyHO(.model=model,
+                .typeHO='refined',
+                .order_indicators = 'exact')
+
+modelHO
+
+out= sem(model = modelHO,data=dataset,orthogonal=T)
+summary(out,standardized=T)
+
+# lav_model=modelcfa
