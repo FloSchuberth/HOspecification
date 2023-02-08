6ab7ffbe079ce9f535aaa6821c52010cb0168dc9
FloSchuberth <f.schuberth@utwente.nl>
2023-02-08 15:57:56 +0100

    fromatting

--- Example.R
+++ Example.R
@@ -2,11 +2,11 @@
 devtools::source_url("https://github.com/FloSchuberth/HOspecification/blob/main/specifyHO.R?raw=TRUE")
 
 model='
- PSS <~ PSS1+PSS2 +PSS3+ PSS4+ PSS5
+PSS <~ PSS1+PSS2 +PSS3+ PSS4+ PSS5
 GRS<~GRS1+ GRS2+GRS3+GRS4+GRS5
 SRS<~SRS1+SRS2+SRS3+SRS4+SRS5
 
-PSS~~GRS + SRS
+PSS~~0*GRS + SRS
 GRS~~SRS
 '
 
